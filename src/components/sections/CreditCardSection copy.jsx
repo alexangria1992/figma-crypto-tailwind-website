153ad9e56a9c02c162de416a61a6be9426443f72
alexangria1992 <alex040892@gmail.com>
2022-11-08 19:00:51 +1300

    Step Section

--- src/components/sections/CreditCardSection copy.jsx
+++ src/components/sections/CreditCardSection copy.jsx
@@ -4,7 +4,7 @@ import SecondaryButton from "../buttons/SecondaryButton";
 import { BsCheckCircleFill } from "react-icons/bs";
 export default function CreditCardSection() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto py-32">
       <div className="grid grid-cols-2  ">
         <img src={creditCard} alt="" />
         <div className="flex justify-center">
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
 import BuyAndTradeSection from "../components/sections/BuyAndTradeSection";
-import CreditCardSection from "../components/sections/CreditCardSection";
+import CreditCardSection from "../components/sections/SecuritySection";
 import CryptocurrencySection from "../components/sections/CryptocurrencySection";
 import HeroSection from "../components/sections/HeroSection";
 import PartnerSection from "../components/sections/PartnerSection";
 import TradingToolsSections from "../components/sections/TradingToolsSections";
+import SecuritySection from "../components/sections/SecuritySection";
+import StepSection from "../components/sections/StepSection";
 
 export default function index() {
   return (
@@ -16,6 +18,8 @@ export default function index() {
       <PartnerSection />
       <CreditCardSection />
       <TradingToolsSections />
+      <SecuritySection />
+      <StepSection />
     </Layout>
   );
 }
